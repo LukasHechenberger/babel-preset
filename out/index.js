10e0197578e5c2f750c32c93e9a92bd8d4c4aaac
CircleCI <circleci@circleci>
2017-11-22 10:49:53 +0000

    chore(release): Add v0.2.0-beta.0 release files [ci skip]

---
+++ out/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.default = function (api, target) {
+  return target === 'browser' ? _browser2.default : _node2.default;
+};
+
+var _node = require('./node');
+
+var _node2 = _interopRequireDefault(_node);
+
+var _browser = require('./browser');
+
+var _browser2 = _interopRequireDefault(_browser);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
